df95aa60189c2cf56952eaaaef843461795dbc4d
Luke Sowinski <lukesowinski@live.co.uk>
2020-09-29 19:29:27 +0100

    Added custom map marker

--- src/components/Map/MapDisplay.js
+++ src/components/Map/MapDisplay.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import L from 'leaflet';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import './MapDisplay.scss';
+
+const PointerIcon = new L.Icon({
+  iconUrl: require('../../images/icon-location.svg'),
+  iconRetinaUrl: require('../../images/icon-location.svg'),
+  iconAnchor: [20, 55],
+  popupAnchor: [0, -35],
+  iconSize: [46, 56],
+  shadowSize: [29, 40],
+  shadowAnchor: [7, 40],
+});
 
 const MapDisplay = () => {
   const point = {
@@ -13,7 +24,7 @@ const MapDisplay = () => {
   return (
     <Map center={position} zoom={14} className="Map">
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={position}>
+      <Marker position={position} icon={PointerIcon}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
